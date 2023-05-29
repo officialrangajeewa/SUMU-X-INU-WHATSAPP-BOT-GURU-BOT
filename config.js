9572f9921625501a4e1280667f55c6a237ce59bd
officialrangajeewa <134950235+officialrangajeewa@users.noreply.github.com>
2023-05-29 20:40:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94706212120', 'sandaru', true],
+  ['94706212120', 'SUMUDU X INU WHATSAPP BOT', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'SUMU X INU WHATSAPP BOT'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
